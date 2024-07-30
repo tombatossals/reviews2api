5337d7cde8f19461a872e3f2ec78963e3b239f66
David Rubert <david.rubert@gmail.com>
2024-07-30 19:13:22 +0200

    update

--- nextjs/src/pages/api/private/products/[...asin].ts
+++ nextjs/src/pages/api/private/products/[...asin].ts
@@ -1,5 +1,4 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
 export const config = {
     runtime: "edge",
@@ -10,19 +9,26 @@ type ResponseData = {
     url?: string,
 }
 
-export default function handler(
-    req: NextApiRequest,
-    res: NextApiResponse<ResponseData>
-) {
-    if (!req) return NextResponse.json({ message: 'No hay petición' }, { status: 400 });
+export default async function handler(
+    req: NextRequest
+): Promise<Response> {
+    const authorization = req.headers.get("authorization");
 
-    const authorization = req?.headers?.get("authorization")
+    const asin = req.url.split("=")[req.url.split("=").length - 1];
     if (!authorization || !authorization.startsWith('Bearer ') || authorization.split(' ')[1] !== "hocuspocus") {
-        return NextResponse.json({ message: 'Necesitas una autorización para ver esto' }, { status: 401 });
+        return NextResponse.json({ message: 'Necesitas una autorización para ver esto' }, {
+            status: 401, headers: {
+                "content-type": "application/json",
+            },
+        });
     }
 
     return NextResponse.json({
         message: 'You did it!',
         url: `https://amazon.es/gp/product/${asin}`,
-    }, { status: 200 });
+    }, {
+        status: 200, headers: {
+            "content-type": "application/json",
+        },
+    });
 }
